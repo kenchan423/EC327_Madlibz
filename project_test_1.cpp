9532a14960ddc0cca42045fd179fb0e668252800
kenchan423 <55323049+kenchan423@users.noreply.github.com>
2021-04-28 02:43:13 +0000

    Add files via upload

--- project_test_1.cpp
+++ project_test_1.cpp
@@ -17,6 +17,10 @@ using std::vector;
 using std::cout;
 using std::cin;
 
+// get rid of text input (white rectangle) after ready to submit
+// change prompt from red to another color
+// issue: wont print final lines if there isnt a blank there, and not every line has a blank --> search for space to know where to put a blank?
+
 size_t writeFunction(void *ptr, size_t size, size_t nmemb, std::string* data) {
   data->append((char*) ptr, size * nmemb);
   return size * nmemb;
@@ -44,34 +48,43 @@ string get_url_contents(string theurl) {
 
 
 int main() {
-  json all_madlibz;
-  // will ask for user input on minlength & maxlength ==> may be add a cool user input button
-  // connect these to buttons?
+  // asking user for max and min length 
   vector<string> max_min;
   max_min.push_back("Maximum");
-  max_min.push_back("Minimum"); 
-  vector<string> answers1;
+  max_min.push_back("Minimum");
+
+  vector<string> user_max_min;
+
+  // window
   sf::RenderWindow window1(sf::VideoMode(800, 800), "How big of a Madlibz?");
+  window1.setFramerateLimit(10);
+
+  sf::Color color(30, 252, 223);
+
+  // submit button
   sf::RectangleShape rectangle1(sf::Vector2f(110.f, 50.f));
   rectangle1.setFillColor(sf::Color::White);
   rectangle1.setPosition(350, 600);
 
+  // text input
   sf::RectangleShape input_box1(sf::Vector2f(110.f, 50.f));
   input_box1.setFillColor(sf::Color::White);
   input_box1.setPosition(350, 300);
 
+  // font
   sf::Font font1;
   font1.loadFromFile("/usr/share/fonts/truetype/ubuntu/Ubuntu-BI.ttf");
+
+  // submit text
   sf::Text text_submit1;
   text_submit1.setFont(font1);
   text_submit1.setString("Submit");
   text_submit1.setFillColor(sf::Color::Blue);
   text_submit1.setStyle(sf::Text::Bold);
   text_submit1.setPosition(350,600);
-  window1.setFramerateLimit(10);
 
+  // user input
   string input_text1;
-
   sf::Text text_input1;
   text_input1.setFont(font1);
   text_input1.setString(input_text1);
@@ -79,14 +92,17 @@ int main() {
   text_input1.setStyle(sf::Text::Bold);
   text_input1.setPosition(350,300);
 
-
   int length_counter = 0;
+
+  // prompt --> what to enter
   sf::Text header_text1;
   header_text1.setFont(font1);
   header_text1.setString(max_min[length_counter]);
   header_text1.setFillColor(sf::Color::Red);
   header_text1.setStyle(sf::Text::Bold);
   header_text1.setPosition(350,200);
+
+  length_counter++;
   while (window1.isOpen())
   {
       sf::Event event1;
@@ -96,7 +112,7 @@ int main() {
               window1.close();
           // if something typed
           else if (event1.type == sf::Event::TextEntered) {
-              if (std::isprint(event1.text.unicode) && length_counter < max_min.size()-1){
+              if (std::isprint(event1.text.unicode) && length_counter < max_min.size()+1){
                   input_text1 += event1.text.unicode;
                    text_input1.setString(input_text1);
               }
@@ -111,11 +127,17 @@ int main() {
               }
               // entering an input
               if (event1.key.code == sf::Keyboard::Return) {
-                      if (length_counter < max_min.size()-1){
-                      answers1.push_back(input_text1);
+
+                      if (length_counter < max_min.size()+1){
+                      user_max_min.push_back(input_text1);
                       text_input1.setString("");
+                      if (length_counter < max_min.size()){
+                        header_text1.setString(max_min.at(length_counter));
+                      }
+                      else {
+                        header_text1.setString("Enter Submit");
+                      }
                       length_counter++;
-                      header_text1.setString(max_min.at(length_counter));
                       input_text1 = "";
                     }
               }
@@ -130,10 +152,8 @@ int main() {
             }
           }
       }
-      window1.clear();
-      if (length_counter < max_min.size()-1){
-        window1.draw(header_text1);
-      }
+      window1.clear(color);
+      window1.draw(header_text1);
       window1.draw(rectangle1);
       window1.draw(text_submit1);
       window1.draw(input_box1);
@@ -141,8 +161,8 @@ int main() {
       window1.display();
   }
 
-  string tempmin = answers1.at(0);
-  string tempmax = answers1.at(1);
+  string tempmin = user_max_min.at(0);
+  string tempmax = user_max_min.at(1);
   string minlength = "minlength=" + tempmin;
   string maxlength = "maxlength=" + tempmax;
   string user_input_length = "?" + minlength + "&" + maxlength;
@@ -150,63 +170,20 @@ int main() {
 
   string resp;
   resp = get_url_contents(madlibz_url);
-  // printing out resp to see if call to api works
-  //cout << resp << '\n';
+
   json current_madlibz = json::parse(resp);
-  //cout << current_madlibz << '\n';
+
   vector<string> questions = current_madlibz["blanks"];
-  int num_output = questions.size();
+  int num_output = current_madlibz["value"].size()-1;
   vector<string> value_str;
   // the last part of value is always a 0 
   // b/c 0 at end of cstrings
-  for (int k=0; k<num_output-1; k++){
+  for (int k=0; k<num_output; k++){
     value_str.push_back(current_madlibz["value"].at(k));
   }
 
-  // not always strings --> so used auto
   string title_str = current_madlibz["title"];
-  /*
-  cout << resp << '\n';
-  for(int p=0; p<num_output-1; p++){
-    cout << value_str[p] << '\n';
-  }
-  */
-  
-  // terminal stuff
-  string temp_str;
-  for (int o=0; o<num_output-1; o++){
-    cout << questions.at(o) << '\n';
-  }
-  
-  /*
-  for (int p=0; p<blank_input.size()-1; p++){
-    cout << blank_input.at(p) << '\n';
-  }
-  */
-  
-  
-
-  all_madlibz.push_back(current_madlibz);
-
-  // saving madlibz into library --> could do an all previous madlibz option
-  std::ofstream mylib;
-  mylib.open("madlibz.json");
-  mylib << all_madlibz << '\n';
-  mylib.close();
-  // seems to overwrite the madlibz.json each time --> want to add to the file
-
-  // cant figure out how to read from madlibz.json
-  json prev_madlibz;
-
-  std::ifstream mylib_rd("madlibz.json", std::ifstream::binary);
-  mylib_rd >> prev_madlibz;
-  //cout << prev_madlibz << '\n';
-  // printing out all previous madlibz --> another button
-
-
-
-  // front end portion
-  
+ 
   vector <string> answers;
   // populate with inputs from API
 
@@ -253,6 +230,8 @@ int main() {
   header_text.setFillColor(sf::Color::Red);
   header_text.setStyle(sf::Text::Bold);
   header_text.setPosition(350,200);
+
+  question_counter++;
   while (window.isOpen())
   {
       sf::Event event;
@@ -262,7 +241,7 @@ int main() {
               window.close();
           // if something typed
           else if (event.type == sf::Event::TextEntered) {
-              if (std::isprint(event.text.unicode) && question_counter < questions.size()-1){
+              if (std::isprint(event.text.unicode) && question_counter < questions.size()+1){
                   input_text += event.text.unicode;
                    text_input.setString(input_text);
               }
@@ -277,11 +256,16 @@ int main() {
               }
               // entering an input
               if (event.key.code == sf::Keyboard::Return) {
-                      if (question_counter < questions.size()-1){
+                      if (question_counter < questions.size()+1){
                       answers.push_back(input_text);
                       text_input.setString("");
+                      if (question_counter < questions.size()){
+                        header_text.setString(questions.at(question_counter));
+                      }
+                      else {
+                        header_text.setString("Enter Submit");
+                      }
                       question_counter++;
-                      header_text.setString(questions.at(question_counter));
                       input_text = "";
                     }
               }
@@ -297,9 +281,7 @@ int main() {
           }
       }
       window.clear();
-      if (question_counter < questions.size()-1){
-        window.draw(header_text);
-      }
+      window.draw(header_text);
       window.draw(rectangle);
       window.draw(text_submit);
       window.draw(input_box);
@@ -310,57 +292,86 @@ int main() {
 
   
   // making the madlibz
+
   string value_temp;
   vector<string> final_madlibz;
   for (int q=0; q<answers.size(); q++){
     value_temp = value_str.at(q) + answers.at(q);
+    cout <<  value_temp << '\n';
     final_madlibz.push_back(value_temp);
     value_temp.clear();
   }
+  
   string madlibz_lite;
+  int temp_len;
+
   for (int c=0; c<answers.size(); c++){
-    // cout << final_madlibz.at(c) << '\n';
     value_temp.clear();
     value_temp = final_madlibz.at(c) + '\n';
     
-    if (value_temp.length() > 40){
-      madlibz_lite.append(value_temp.substr(0,20));
-      cout << value_temp.substr(0,20);
+    if (value_temp.length() > 80){
+      temp_len = value_temp.length()/2;
+      madlibz_lite.append(value_temp.substr(0, temp_len));
       madlibz_lite.append("\n");
-      madlibz_lite.append(value_temp.substr(21, value_temp.length()-1));
-      cout << value_temp.substr(21, value_temp.length()-1);
+      madlibz_lite.append(value_temp.substr(temp_len-1, temp_len));
       continue; 
     }
     
     madlibz_lite.append(value_temp);
   }
-  cout << madlibz_lite;
+  madlibz_lite.append(value_str.at(num_output-1));
   
   // 2nd terminal --> displaying the final madlibz
-  sf::RenderWindow window2(sf::VideoMode(1000, 800), "Madlibz Lite");
+  sf::RenderWindow window2(sf::VideoMode(2500, 1000), "Madlibz Lite");
   sf::Text madlibz_str;
 
   madlibz_str.setString(madlibz_lite);
   madlibz_str.setFont(font);
   madlibz_str.setPosition(350,100);
   // can try to do if statements and make this more responsive
-  madlibz_str.setCharacterSize(15);
+  madlibz_str.setCharacterSize(24);
 
   // customizing text appearance
-  madlibz_str.setFillColor(sf::Color::Red);
-  madlibz_str.setOutlineColor(sf::Color::Blue);
+  madlibz_str.setFillColor(sf::Color::White);
+  // madlibz_str.setOutlineColor(sf::Color::Blue);
   madlibz_str.setOutlineThickness(2);
   madlibz_str.setStyle(sf::Text::Bold);
 
   window.setFramerateLimit(10);
   while (window2.isOpen())
   {
+    sf::Event event2;
+    while (window2.pollEvent(event2))
+    {
+      if (event2.type == sf::Event::Closed)
+              window2.close();
+    }
     madlibz_str.setPosition(100,100);
     window2.draw(madlibz_str);
     window2.display();
   }
   
+  // previous madlibs
 
+  /*
+  // saving madlibz into library --> could do an all previous madlibz option
+  
+  json all_madlibz;
+  all_madlibz.push_back(current_madlibz);
+
+  std::ofstream mylib;
+  mylib.open("madlibz.json");
+  mylib << all_madlibz << '\n';
+  mylib.close();
+  // seems to overwrite the madlibz.json each time --> want to add to the file
+
+  // cant figure out how to read from madlibz.json
+  json prev_madlibz;
+
+  std::ifstream mylib_rd("madlibz.json", std::ifstream::binary);
+  mylib_rd >> prev_madlibz;
+
+  */
 
   return 0;
 }
